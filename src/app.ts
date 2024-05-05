67f27c67aea0c799392971aa475beb6a80a586d3
Tiago-92 <tiagoamaralcbei@hotmail.com>
2024-05-05 19:18:10 -0300

    feat: rota para cadastro de pets

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,44 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/pets', async (request, reply) => {
+  const usersBodySchema = z.object({
+    name: z.string(),
+    about: z.string(),
+    age: z.string(),
+    energyLevel: z.string(),
+    animalSize: z.string(),
+    independeceLevel: z.string(),
+    photo: z.string(),
+    spacious: z.string(),
+  })
+
+  const {
+    name,
+    about,
+    age,
+    energyLevel,
+    animalSize,
+    independeceLevel,
+    photo,
+    spacious,
+  } = usersBodySchema.parse(request.body)
+
+  await prisma.pet.create({
+    data: {
+      name,
+      about,
+      age,
+      energyLevel,
+      animalSize,
+      independeceLevel,
+      photo,
+      spacious,
+    },
+  })
+
+  return reply.status(201).send()
+})
